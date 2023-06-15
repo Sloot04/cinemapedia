d5a48bd4a6435b18b4eae502f11aacbadcc7ccfd
Suyai Slootmans <suyaislootmans@gmail.com>
2023-06-15 10:49:07 -0300

    datasource

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,10 +7,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.theMovieDbKey),
-      ),
-    );
+    return const Scaffold(body: Placeholder());
   }
 }
