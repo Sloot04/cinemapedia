613f1f5c556b58cdf90b1cf57bc27794bb0a6be9
Suyai Slootmans <suyaislootmans@gmail.com>
2023-07-04 10:42:20 -0300

    datasource y repository - SearchMovies

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -35,18 +35,18 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> getPopular({int page = 1}) async {
-     final response =
+    final response =
         await dio.get('/movie/popular', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
   }
-  
+
   @override
   Future<List<Movie>> getTopRated({int page = 1}) async {
     final response =
         await dio.get('/movie/top_rated', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
   }
-  
+
   @override
   Future<List<Movie>> getUpcoming({int page = 1}) async {
     final response =
@@ -55,11 +55,18 @@ class MoviedbDatasource extends MoviesDatasource {
   }
 
   @override
-  Future<Movie> getMovieById (String id) async {
-     final response = await dio.get('/movie/$id');
-     if (response.statusCode != 200) throw Exception('Movie with id : $id not found');
-     final movieDetails = MovieDetails.fromJson(response.data);
-     final Movie movie = MovieMapper.movieDatailToEntity(movieDetails);
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get('/movie/$id');
+    if (response.statusCode != 200) throw Exception('Movie with id : $id not found');
+    final movieDetails = MovieDetails.fromJson(response.data);
+    final Movie movie = MovieMapper.movieDatailToEntity(movieDetails);
     return movie;
+  }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+    return _jsonToMovies(response.data);
   }
 }
