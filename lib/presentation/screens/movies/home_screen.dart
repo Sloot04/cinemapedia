d850ed4f705993fb3ecc80571d347de74589b0ec
Suyai Slootmans <suyaislootmans@gmail.com>
2023-07-25 15:21:23 -0300

    configuracion del Router

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -4,15 +4,20 @@ import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[HomeView(), SizedBox(), FavoriteView()];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return  Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar:const  CustomBottomNavigation(),
     );
   }
 }
-
