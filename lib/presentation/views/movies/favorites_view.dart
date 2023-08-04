563c6e716758c8c5597bbab9b1578fc00adc19ee
Suyai Slootmans <suyaislootmans@gmail.com>
2023-08-04 13:49:21 -0300

    mostrar peliculas favoritas

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
-class FavoriteView extends StatelessWidget {
+class FavoriteView extends ConsumerStatefulWidget {
   const FavoriteView({super.key});
 
   @override
+  FavoriteViewState createState() => FavoriteViewState();
+}
+
+class FavoriteViewState extends ConsumerState<FavoriteView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites view'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
-      ),
-    );
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+    return Scaffold(body: ListView.builder(
+      itemCount: favoriteMovies.length,
+      itemBuilder: (context, index) {
+      final favoriteMovie = favoriteMovies[index];
+      return ListTile(
+        title: Text(favoriteMovie.title),
+      );
+    }));
   }
 }
