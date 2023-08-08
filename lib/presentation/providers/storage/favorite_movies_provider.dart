acd1b2d5a6ab4a86f99f90a5cf8b69f882df7deb
Suyai Slootmans <suyaislootmans@gmail.com>
2023-08-08 11:08:34 -0300

    remover y agregar elementos

--- lib/presentation/providers/storage/favorite_movies_provider.dart
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -16,7 +16,8 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
   StorageMoviesNotifier({required this.localStorageRepository}) : super({});
 
   Future<List<Movie>> loadNextPage() async {
-    final movies = await localStorageRepository.loadMovies(offset: page * 10);
+    final movies =
+        await localStorageRepository.loadMovies(offset: page * 10, limit: 20);
     page++;
     final tempMoviesMap = <int, Movie>{};
     for (final movie in movies) {
@@ -24,5 +25,17 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
     }
     state = {...state, ...tempMoviesMap};
     return movies;
+  }
+
+  Future<void> toggleFavorite(Movie movie) async {
+    await localStorageRepository.toggleFavorite(movie);
+    final bool isMovieInFavorite = state[movie.id] != null;
+
+    if (isMovieInFavorite) {
+      state.remove(movie.id);
+      state = {...state};
+    } else {
+      state = {...state, movie.id: movie};
+    }
   }
 }
